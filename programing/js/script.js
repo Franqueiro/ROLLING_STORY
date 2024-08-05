0f2ffb881e66f255655c5cda32b5be1fcaaa16ee
Felipe Spider <feliprerobinsonprofissional@gmail.com>
2024-08-05 19:18:29 -0300

    Update

--- programing/js/script.js
+++ programing/js/script.js
@@ -5,21 +5,21 @@ avanca.forEach(button => {
         const atual = document.querySelector('.ativo');
         let proximoPasso = 'passo-' + this.getAttribute('data-proximo');
         
-        // Check if the current step is passo-4 or passo-6
-        if (atual.id === 'passo-4' || atual.id === 'passo-6') {
+        if (atual.id === 'passo-4' || 'passo-6') {
+            let doisPassos = 'passo-'
             const numeroAleatorio = Math.random();
             const resultado = Math.floor(numeroAleatorio * 2) + 1;
             
             if (resultado % 2 === 0) {
-                proximoPasso
-            } 
+                atual.classList.remove('ativo');
+                document.getElementById(proximoPasso).classList.add('ativo');
+            }
             else {
-                const proximoPassoNumero = parseInt(this.getAttribute('data-proximo'), 10) + 1;
-                proximoPasso = 'passo-' + proximoPassoNumero;
-                console.log("Número ímpar, pula para o próximo passo.");
+                atual.classList.remove('ativo');
+                document.getElementById()
             }
         }
-        
+
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
     });
