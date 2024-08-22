69de79b0ce1c0694b4fb6fc1ed73910ad28977c5
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-08-21 23:00:20 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        let proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-        
-        if (atual.id === 'passo-4' || 'passo-6') {
-            let doisPassos = 'passo-'
-            const numeroAleatorio = Math.random();
-            const resultado = Math.floor(numeroAleatorio * 2) + 1;
-            
-            if (resultado % 2 === 0) {
-                atual.classList.remove('ativo');
-                document.getElementById(proximoPasso).classList.add('ativo');
-            }
-            else {
-                atual.classList.remove('ativo');
-                document.getElementById()
-            }
-        }
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
