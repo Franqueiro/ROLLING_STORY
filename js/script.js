d7ceccbc65572192e29fd9344b704552118ba866
FELIPE DA COSTA ROBINSON <u24464@academico.cotuca.unicamp.br>
2024-08-24 09:51:26 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const avanca = document.querySelectorAll('.btn-proximo');
+const closeButtons = document.querySelectorAll('.close-button');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
@@ -7,5 +8,13 @@ avanca.forEach(button => {
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
+    });
+});
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        atual.classList.remove('ativo');
+        document.getElementById('passo-0').classList.add('ativo');
     });
 });
